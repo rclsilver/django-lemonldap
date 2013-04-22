aeba9e3c6d2beb9f1ef6d109c95b1bf282174c34
Thomas Bétrancourt <thomas@betrancourt.net>
2013-04-22 23:15:58 +0200

    Affichage des paramètres supplémentaires dans la page de débogage

--- lemonldap/views.py
+++ lemonldap/views.py
@@ -7,6 +7,9 @@ def debug(request):
         raise SuspiciousOperation("This view isn't available in production mode! Please set DEBUG to True")
     auth_headers = {}
     auth_headers['username'] = request.META.get('HTTP_AUTH_USER', None)
+    auth_headers['firstname'] = request.META.get('HTTP_AUTH_FIRSTNAME', None)
+    auth_headers['lastname'] = request.META.get('HTTP_AUTH_LASTNAME', None)
+    auth_headers['mail'] = request.META.get('HTTP_AUTH_MAIL', None)
     auth_headers['groups'] = request.META.get('HTTP_AUTH_GROUPS', '').split('; ')
     auth_headers['is_superuser'] = request.META.get('HTTP_AUTH_SUPERUSER', None) == 'true'
     auth_headers['is_staff'] = request.META.get('HTTP_AUTH_STAFF', None) == 'true'
