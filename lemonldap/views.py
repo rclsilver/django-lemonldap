7c5183995b7732a19fa162a5cba802116449a3c9
Thomas Bétrancourt <thomas@betrancourt.net>
2013-04-22 22:49:17 +0200

    Ajout des entêtes HTTP spéciales fournies par LL:NG : le nom des entêtes est pour le moment défini en dur dans le code

--- lemonldap/views.py
+++ lemonldap/views.py
@@ -5,4 +5,9 @@ from django.shortcuts import render
 def debug(request):
     if not settings.DEBUG:
         raise SuspiciousOperation("This view isn't available in production mode! Please set DEBUG to True")
-    return render(request, 'lemonldap/debug.html')
+    auth_headers = {}
+    auth_headers['username'] = request.META.get('HTTP_AUTH_USER', None)
+    auth_headers['groups'] = request.META.get('HTTP_AUTH_GROUPS', '').split('; ')
+    auth_headers['is_superuser'] = request.META.get('HTTP_AUTH_SUPERUSER', None) == 'true'
+    auth_headers['is_staff'] = request.META.get('HTTP_AUTH_STAFF', None) == 'true'
+    return render(request, 'lemonldap/debug.html', {'auth_headers': auth_headers})
