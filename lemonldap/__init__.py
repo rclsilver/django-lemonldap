2daa3b3fd9ece76220ebee93eef8eb606d9bfccc
Thomas Bétrancourt <thomas@betrancourt.net>
2013-04-23 18:56:26 +0200

    Ajout des paramètres de description pour le module lemonldap

---
+++ lemonldap/__init__.py
@@ -0,0 +1,9 @@
+"""
+lemonldap - Allow users to be authenticated through LemonLDAP::NG in django applications
+"""
+
+__version__ = "1.0.0"
+__authors__ = [
+	"Thomas Betrancourt <thomas@betrancourt.net>",
+]
+
