cb80a52154c1b10c0ac94c8ff1c30de56f054d88
Thomas Bétrancourt <thomas@betrancourt.net>
2013-04-23 10:06:46 +0200

    Utilisation de la liste des entêtes dans la page de débogage

--- lemonldap/views.py
+++ lemonldap/views.py
@@ -1,15 +1,12 @@
 from django.conf import settings
 from django.core.exceptions import SuspiciousOperation
 from django.shortcuts import render
+from lemonldap.auth.middleware import LemonldapAuthenticationMiddleware
 
 def debug(request):
     if not settings.DEBUG:
         raise SuspiciousOperation("This view isn't available in production mode! Please set DEBUG to True")
     auth_headers = {}
-    auth_headers['username'] = request.META.get('HTTP_AUTH_USER', None)
-    auth_headers['firstname'] = request.META.get('HTTP_AUTH_FIRSTNAME', None)
-    auth_headers['lastname'] = request.META.get('HTTP_AUTH_LASTNAME', None)
-    auth_headers['mail'] = request.META.get('HTTP_AUTH_MAIL', None)
-    auth_headers['is_superuser'] = request.META.get('HTTP_AUTH_SUPERUSER', None) == 'true'
-    auth_headers['is_staff'] = request.META.get('HTTP_AUTH_STAFF', None) == 'true'
+    for header in LemonldapAuthenticationMiddleware.headers:
+        auth_headers[header[0]] = request.META.get(header[1], None)
     return render(request, 'lemonldap/debug.html', {'auth_headers': auth_headers})
