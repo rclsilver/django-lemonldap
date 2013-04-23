4b47cf5c9c4471dc53333b601d3ed3b45f2ae146
Thomas Bétrancourt <thomas@betrancourt.net>
2013-04-23 10:06:24 +0200

    Création d'un tableau référenceant les différentes entêtes HTTP utilisées

--- lemonldap/auth/middleware.py
+++ lemonldap/auth/middleware.py
@@ -1,3 +1,12 @@
 class LemonldapAuthenticationMiddleware(object):
+    headers = [
+        ('username', 'HTTP_AUTH_USER'),
+        ('firstname', 'HTTP_AUTH_FIRSTNAME'),
+        ('lastname', 'HTTP_AUTH_LASTNAME'),
+        ('mail', 'HTTP_AUTH_MAIL'),
+        ('is_superuser', 'HTTP_AUTH_SUPERUSER'),
+        ('is_staff', 'HTTP_AUTH_STAFF'),
+    ]
+    
     def process_request(self, request):
         pass
