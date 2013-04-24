c4b6192d9ef63e448c7a402cbb77ee9edc1e59bd
Thomas Bétrancourt <thomas@betrancourt.net>
2013-04-24 13:48:53 +0200

    Correction d'un bug dans la méthode configure_user() qui provoquait des erreurs lorsque des paramètres facultatifs n'étaient pas fournis"

--- lemonldap/auth/backends.py
+++ lemonldap/auth/backends.py
@@ -45,9 +45,9 @@ class LemonldapUserBackend(RemoteUserBackend):
         """
         Configures a user after creation and returns the updated user.
         """
-        user.email = user_infos['mail']
-        user.first_name = user_infos['firstname']
-        user.last_name = user_infos['lastname']
+        if user_infos['mail']: user.email = user_infos['mail']
+        if user_infos['firstname']: user.first_name = user_infos['firstname']
+        if user_infos['lastname']: user.last_name = user_infos['lastname']
         user.is_superuser = 'true' == user_infos['is_superuser'] or user_infos['is_superuser'] is True
         user.is_staff = 'true' == user_infos['is_staff'] or user_infos['is_staff'] is True
         user.save()
