4c8441143e2c18e512a4979b39a7124c6c0ec91b
Thomas Bétrancourt <thomas@betrancourt.net>
2013-04-24 13:50:57 +0200

    Mise à jour de la variable __version__

--- lemonldap/__init__.py
+++ lemonldap/__init__.py
@@ -2,7 +2,7 @@
 lemonldap - Allow users to be authenticated through LemonLDAP::NG in django applications
 """
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __authors__ = [
 	"Thomas Betrancourt <thomas@betrancourt.net>",
 ]
