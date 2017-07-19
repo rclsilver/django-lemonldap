5bca4b0b6550aa9bd3bbe49dba3ba1d885c78305
Thomas Bétrancourt <thomas@betrancourt.net>
2017-07-19 09:56:04 +0200

    Bump to 2.0.0 (compatibility with django >= 1.10)

--- lemonldap/__init__.py
+++ lemonldap/__init__.py
@@ -2,7 +2,7 @@
 lemonldap - Allow users to be authenticated through LemonLDAP::NG in django applications
 """
 
-__version__ = "1.0.1"
+__version__ = "2.0.0"
 __authors__ = [
 	"Thomas Betrancourt <thomas@betrancourt.net>",
 ]
